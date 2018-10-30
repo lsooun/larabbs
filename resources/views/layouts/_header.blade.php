5b833fc16c8e000daae1a934ebcc7ff44802a08c
lsooun <lsooun@gmail.com>
2018-10-30 15:31:26 +0000

    Integrated administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -53,6 +53,17 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+
+                            @can('manage_contents')
+                                <li>
+                                    <a href="{{ url(config('administrator.uri')) }}">
+                                        <span class="glyphicon glyphicon-dashboard" aria-hidden="true">
+                                            管理后台
+                                        </span>
+                                    </a>
+                                </li>
+                            @endcan
+
                             <li>
                                 <a href="{{ route('users.show', Auth::id()) }}">
                                     <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
