fcd53739f4994ba0b6550759923a64f71aa6d7e1
lsooun <lsooun@gmail.com>
2018-11-18 23:27:04 +0800

    up

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\Api\UserRequest;
+
+class UsersController extends Controller
+{
+    public function store(UserRequest $request)
+    {
+
+    }
+}
